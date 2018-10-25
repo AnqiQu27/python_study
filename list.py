689ca4b5f5a1f633abfe039936e4304a5be27155
anqi <anqi.qu.eea@gmail.com>
2018-10-25 09:23:11 +0900

    practised list

---
+++ list.py
@@ -0,0 +1,22 @@
+guest = ["weiwei", "dog", "cat"]
+cant_join = "dog"
+print(guest)
+print(cant_join)
+guest.remove(cant_join)
+guest.append("cookie")
+print(guest)
+guest.insert(0,'mew')
+guest.insert(2, 'wan')
+guest.append("elephant")
+print(guest)
+guest.pop()
+guest.pop()
+guest.pop()
+guest.pop()
+print(guest)
+del guest[0]
+del guest[0]
+print(guest)
+
+
+
